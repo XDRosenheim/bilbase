22cf3f155a2ae78a9a9944bdbd6b37d8a9b5b39c
Rosenheim <XDRosenheim@users.noreply.github.com>
2015-05-08 08:55:14 +0200

    Logout.
    HELP!

--- MasterPage.master.cs
+++ MasterPage.master.cs
@@ -7,6 +7,14 @@ public partial class MasterPage : System.Web.UI.MasterPage
         if (Session["UserID"] != null && Session["UserID"].ToString() == "1")
         {
             AdminLink.Visible = true;
+            LoginHyper.Visible = false;
+            LogoutHyper.Visible = true;
         }
+        else if (Session["UserID"] != null)
+        {
+            LoginHyper.Visible = false;
+            LogoutHyper.Visible = true;
+        }
+        LogoutHyper.Visible = false;
     }
 }
